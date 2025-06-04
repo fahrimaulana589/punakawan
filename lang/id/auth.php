238a2cc18880cd85749f58693a95c60cf12aaf82
fahri <akhmadfahri589@gmail.com>
2025-06-04 16:50:05 +0700

    set ke waktu indonesia

---
+++ lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Autentikasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentikasi untuk berbagai
+    | pesan yang perlu ditampilkan kepada pengguna. Anda bebas untuk
+    | memodifikasi baris bahasa ini sesuai kebutuhan aplikasi Anda.
+    |
+    */
+
+    'failed' => 'Kredensial ini tidak cocok dengan catatan kami.',
+    'password' => 'Kata sandi yang diberikan salah.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Silakan coba lagi dalam :seconds detik.',
+
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Reset Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah baris standar yang cocok dengan alasan
+    | yang diberikan oleh broker kata sandi saat upaya penggantian kata
+    | sandi gagal, seperti token tidak valid atau kata sandi baru tidak sah.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah berhasil direset.',
+    'sent' => 'Kami telah mengirimkan tautan untuk mereset kata sandi Anda melalui email.',
+    'throttled' => 'Silakan tunggu sebelum mencoba lagi.',
+    'token' => 'Token reset kata sandi ini tidak valid.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+
+];
