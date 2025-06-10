9f1796585ac8b9c246060a44109ca1a9e81074bb
fahri <akhmadfahri589@gmail.com>
2025-06-10 16:57:08 +0700

    perubahan terakhir terkait persedian, belum selesai

---
+++ app/Http/Controllers/PersedianProdukController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PersedianProduk;
+use Illuminate\Http\Request;
+
+class PersedianProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PersedianProduk $persedianProduk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PersedianProduk $persedianProduk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PersedianProduk $persedianProduk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PersedianProduk $persedianProduk)
+    {
+        //
+    }
+}
